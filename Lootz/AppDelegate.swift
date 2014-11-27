a2541aca415cba710382ee4b48c9faa731b5f768
BraydenGirard <braydengirard@cmail.carleton.ca>
2014-11-27 17:08:52 -0500

    Added parse framework.

--- Lootz/AppDelegate.swift
+++ Lootz/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.setApplicationId("vVMocViAhExgXuYFlgybvH3t3ZtEqYIpsdY2qFww", clientKey: "kv6nYcTUuu6YljaxVclt1BiMlV0z95gFGdzrdWTj")
+        PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
         return true
     }
 
