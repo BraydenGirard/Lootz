b43300de5bf1e865b10641128544609d1cd6e02d
BraydenGirard <braydengirard@cmail.carleton.ca>
2014-11-27 16:43:10 -0500

    Initial commit from local git'

---
+++ LootzTests/LootzTests.swift
@@ -0,0 +1,36 @@
+//
+//  LootzTests.swift
+//  LootzTests
+//
+//  Created by Brayden Girard on 2014-11-27.
+//  Copyright (c) 2014 Brayden Girard. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LootzTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
