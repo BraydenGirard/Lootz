f636bbe1d4366eb14f4566f3630c5d336dfe5e57
Brayden Girard <braydengirard@Braydens-MacBook-Pro.local>
2014-12-05 17:26:41 -0500

    Added login and signup

--- SignupController.swift
+++ SignupController.swift
@@ -1,29 +1,33 @@
 //
-//  ViewController.swift
+//  SignupController.swift
 //  Lootz
 //
-//  Created by Brayden Girard on 2014-11-27.
+//  Created by Brayden Girard on 2014-12-05.
 //  Copyright (c) 2014 Brayden Girard. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SignupController: UIViewController {
+    
+    @IBOutlet var encryptButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backPushed(sender : UIButton) {
+       dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         self.view.endEditing(true)
     }
-
+    
 }
-
